4657cf2c58b5b58edbec9af2510474821bb4e2ec
Anubhav Kushwaha <k04kushwaha@gmail.com>
2022-10-14 21:47:57 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
-    uvicorn.run("zenik_backend.asgi:application", port=8121, log_level='info')
+    uvicorn.run("k4api.wsgi:application", port=8121, log_level='info')
