ba99b381a77d2a4ace8ab9b7a8ff68527a66bd7e
Anubhav Kushwaha <k04kushwaha@gmail.com>
2022-10-15 17:18:27 +0530

    moved apps to their own repo

--- k4api/urls.py
+++ k4api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("spotify/", include("spotify.urls")),
+    path("spotify/", include("spotify_banner.urls")),
 ]
--- spotify/exceptions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.exceptions import APIException
-
-
-class NoTrackPlaying(APIException):
-    status_code = 204
-    default_detail = 'No track is playing.'
-    default_code = 'no_track_playing'
