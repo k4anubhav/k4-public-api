f622828f787d66135e5e66da2e0834ecbd6c25ee
Anubhav Kushwaha <k04kushwaha@gmail.com>
2023-01-23 19:21:08 +0530

    fix prod settings

--- k4api/settings.py
+++ k4api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-^=$s_9a6i7yf&0@#0@+wut##cl^+m+brn*0p)@m$a5z&lkow@)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ["ALLOWED_HOSTS"]] if os.environ.get("ALLOWED_HOSTS") else []
+ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]] if os.environ.get("WEBSITE_HOSTNAME") else []
 
 # Application definition
 
