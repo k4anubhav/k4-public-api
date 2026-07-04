370836cf3783e7a730dc13573e7eecce61b09ae0
Anubhav K. <me@k4anubhav.com>
2026-07-05 03:16:39 +0530

    fix db persistence

--- k4api/k4api/settings.py
+++ k4api/k4api/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = "k4api.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "data" / "db.sqlite3",
     }
 }
 
